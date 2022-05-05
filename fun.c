892b93987668fca7d16691b643ccc5aac20b9143
Anurag Kumar Tiwari <98815562+AnuragTiwari2000@users.noreply.github.com>
2022-05-05 12:43:56 +0530

    Update fun.c

--- fun.c
+++ fun.c
@@ -1,1 +1,17 @@
+#include"fun.h"
+#include<string.h>
+#include<stdio.h>
+int name(char str[])
+{
+    char *p=str;
+    int sum=0, length;
+    length= strlen(str);
+    int arr[length];
+    for(int i=0; i<=length;i++)
+    {
+        arr[i]=(int)*(p+i);
+        sum=sum+arr[i];
+    }
+    return sum;
 
+}
